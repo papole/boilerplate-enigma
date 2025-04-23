af871f411ae530bb8f474079b6677a089d9c3225
2_A5muo8kPg7h6LurUAj <ezequiel1507@gmail.com>
2025-04-23 07:52:47 -0300

    add: I add missing tests

--- api/test/products.e2e-spec.ts
+++ api/test/products.e2e-spec.ts
@@ -3,8 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+
+
 describe('ProductController (e2e)', () => {
   let app: INestApplication;
+  let data: any = []
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,19 +18,21 @@ describe('ProductController (e2e)', () => {
     await app.init();
   });
 
-  it('/product (GET)', () => {
+  it('/product (GET) - 200', () => {
     return request(app.getHttpServer())
       .get('/product?limit=20&page=1')
       .expect(200)
       .expect((res: request.Response) => {
+        data = res.body.data
         //como body no es un array debo comentar esta linea 
-        //expect(Array.isArray(res.body)).toBe(true);
-        expect(Array.isArray(res.body.data)).toBe(true);
+        //expect(Array.isArray(res.body)).toBe(true);        
+        expect(Array.isArray(res.body.data)).toBe(true);        
         expect(typeof res.body.total).toBe('number');
+        
       });
-  });
+  });  
 
-  it('/product (POST)', () => {
+  it('/product (POST) - 200', () => {
     return request(app.getHttpServer())
       .post('/product')
       .send({ name: 'Camisa', sku: '123', stock: 10 })
@@ -36,6 +41,52 @@ describe('ProductController (e2e)', () => {
         expect(res.body).toHaveProperty('id');
       });
   });
+
+  it('/product (PATH) - 404', () => {
+    return request(app.getHttpServer())
+      .patch(`/product/abdc?typeMovement=IN`)
+      .send({ stock: 10 })
+      .expect(404)
+      .expect((res: request.Response) => {
+        expect(res.body).toHaveProperty('statusCode', 404);
+        expect(res.body).toHaveProperty('message');
+      });
+  });
+
+  it('/product (PATH) STOCK OUT - 400', () => {
+    return request(app.getHttpServer())
+      .patch(`/product/${data[0].id}?typeMovement=OUT`)
+      .send({ stock: 10000 })
+      .expect(400)
+      .expect((res: request.Response) => {
+        expect(res.body).toHaveProperty('statusCode', 400);
+        expect(res.body).toHaveProperty('message');
+      });
+  });
+
+  it('/product (PATH) STOCK IN - 200', () => {
+    return request(app.getHttpServer())
+      .patch(`/product/${data[0].id}?typeMovement=IN`)
+      .send({ stock: 8 })
+      .expect(200)
+      .expect((res: request.Response) => {
+        expect(res.body).toHaveProperty('id');
+      });
+  });
+
+  it('/stock-movement (GET) - 200', () => {
+    return request(app.getHttpServer())
+      .get('/stock-movement?limit=20&page=1')
+      .expect(200)
+      .expect((res: request.Response) => {
+        data = res.body.data
+        //como body no es un array debo comentar esta linea 
+        //expect(Array.isArray(res.body)).toBe(true);        
+        expect(Array.isArray(res.body.data)).toBe(true);        
+        expect(typeof res.body.total).toBe('number');
+        
+      });
+  }); 
 
   afterAll(async () => {
     await app.close();
